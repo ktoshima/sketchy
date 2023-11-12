a3210742b8eb99b42ae9317b59d57b780860dfa7
ktoshima <24969845+ktoshima@users.noreply.github.com>
2023-11-13 00:12:04 +0900

    setup popup

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // pages and components
 import InputForm from './pages/InputForm';
 import Session from './pages/Session';
+import Popup from './pages/Popup';
 import Navbar from './components/Navbar';
-
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <Popup />
         <div className="pages">
           <Routes>
             <Route
               exact path='/'
+              element={ <Popup /> }
+            />
+            <Route
+              path='/input-form'
               element={ <InputForm /> }
             />
             <Route
