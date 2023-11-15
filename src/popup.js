47ad7b0465fe619931b1ddb08754037ba956b714
ktoshima <24969845+ktoshima@users.noreply.github.com>
2023-11-15 22:28:10 +0900

    add message sender and listener for createGallery and openSession

--- src/popup.js
+++ src/popup.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-
-import { useState } from 'react';
+// import './popup.css';
 
 const Popup = () => {
 	const [tabURL, setTabURL] = useState(null);
 	const [tabId, setTabId] = useState(null);
 	const [invalidURL, setInvalidURL] = useState(false);
 	const [isCreatingGallery, setIsCreatingGallery] = useState(false)
+	const [gallery, setGallery] = useState([]);
+
+	useEffect(() => {
+		console.log('re-rendered');
+	}, [isCreatingGallery]);
 
 	const createGallery = () => {
 		setIsCreatingGallery(true);
 		browser.scripting.executeScript({
 			target: {tabId: tabId},
 			files: ['./createGallery.js']
-		}).then((result) => {
-			console.log(result);
-			setIsCreatingGallery(false);
-		})
+		});
+	}
 
+	const openSession = () => {
+		// send background.js message to open index.html
+		browser.runtime.sendMessage({
+			type: "open_session",
+			gallery: gallery
+		})
+		return null;
 	}
 
 	browser.tabs.query({active: true, currentWindow: true})
@@ -33,12 +42,21 @@ const Popup = () => {
 			}
 		})
 
+	browser.runtime.onMessage.addListener((data) => {
+		if (data.type === "gallery_ready") {
+			setGallery(data.gallery);
+			setIsCreatingGallery(false);
+		}
+	})
+
 	return (
 		<>
 			{ tabURL && (
 				<>
-					<div className="start">Query: { tabURL.searchParams.get('q') } </div>
-					<button disabled={isCreatingGallery} onClick={() => createGallery()}>Start</button>
+					<div className="query">Query: { tabURL.searchParams.get('q') } </div>
+					<button disabled={isCreatingGallery} onClick={() => {createGallery();}}>Create Gallery</button>
+					<div className="gallery">Gallery length: { gallery.length } </div>
+					<button disabled={!gallery.length} onClick={() => {openSession();}}>Open session</button>
 				</>
 			) }
 			{ invalidURL && (
