7f1cd85906dc233807b2a27c3210a6f262a9e916
ktoshima <24969845+ktoshima@users.noreply.github.com>
2023-11-24 11:52:43 +0900

    add fontsource to use Roboto

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './popup.css';
+
+// font import for material ui
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const Popup = () => {
 	const [tabURL, setTabURL] = useState(null);
