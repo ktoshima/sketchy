f33eb051cbee7282e48ff1dc83875339bbdf12a8
ktoshima <24969845+ktoshima@users.noreply.github.com>
2023-12-17 01:08:20 +0900

    add ntr font to popup

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../../assets/styles/index.css';
+import "@fontsource/ntr/400.css";
 import App from './App';
 import { SessionContextProvider } from './context/SessionContext';
 
