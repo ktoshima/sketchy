eb065a61b8a95094adfff8c87ce57ff2b31d4e3c
ktoshima <24969845+ktoshima@users.noreply.github.com>
2023-12-30 00:52:20 +0900

    add bg component for app

---
+++ src/pages/App/components/Background.js
@@ -0,0 +1,9 @@
+import AppBG from '../../../assets/images/app_bg.svg';
+
+const Background = () => (
+	<div className="background-layer">
+		<img src={AppBG} alt="" />
+	</div>
+);
+
+export default Background;
