f74f442dba5c76b46865c2763a6c016dbf2411ae
ktoshima <24969845+ktoshima@users.noreply.github.com>
2024-01-11 08:53:08 +0900

    generalize browser config setup and add comments

--- webpack.config.js
+++ webpack.config.js
@@ -60,19 +60,11 @@ module.exports = (env, argv) => {
 		path: path.resolve(__dirname, 'dist', env.browser),
 		filename: '[name].bundle.js',
 	}
+
+	// browser type config
 	// until chrome 121 rolls out, separate manifest file is neccesary to avoid Chrome refusing to load manifest with background.scripts
-	if (env.browser === 'firefox') {
-		config.plugins.push(
-			new CopyPlugin({
-				patterns: [
-					{
-						from: './src/manifest.json',
-						to: path.join(__dirname, 'dist', env.browser, 'manifest.json')
-					},
-				]
-			})
-		)
-	} else if (env.browser === 'chrome') {
+	// latest version of Firefox and Safari supports both bachground.scripts and background.service_worker being in manifest.json
+	if (env.browser === 'chrome') {
 		config.plugins.push(
 			new CopyPlugin({
 				patterns: [
@@ -83,8 +75,21 @@ module.exports = (env, argv) => {
 				]
 			})
 		)
+	} else  {
+		config.plugins.push(
+			new CopyPlugin({
+				patterns: [
+					{
+						from: './src/manifest.json',
+						to: path.join(__dirname, 'dist', env.browser, 'manifest.json')
+					},
+				]
+			})
+		)
 	}
 	// compile mode config
+	// use inline-source-map in development mode for debug purpose
+	// use MiniCssExtractPluging in production mode to separate css files for optimization
 	if (argv.mode === 'development') {
 		config.devtool = 'inline-source-map';
 		config.module.rules.push(
