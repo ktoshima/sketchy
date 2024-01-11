dcd49bfa72c3daf6160129c197c6135e26c35f00
ktoshima <24969845+ktoshima@users.noreply.github.com>
2024-01-11 22:25:26 +0900

    add noto sans jp to js and css

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../../assets/styles/app.scss';
 import "@fontsource/ntr/400.css";
+import "@fontsource/noto-sans-jp/400.css";
 import App from './App';
 import { SessionContextProvider } from './context/SessionContext';
 import "../../i18n/config";
--- src/pages/Popup/index.js
+++ src/pages/Popup/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '../../assets/styles/popup.scss';
 import "@fontsource/ntr/400.css";
 import "@fontsource/simonetta/400.css";
+import "@fontsource/noto-sans-jp/400.css";
 import Popup from './Popup';
 import "../../i18n/config";
 
