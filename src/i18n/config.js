3576aaa5bbf582db73d8bad7df8291db6839be18
ktoshima <24969845+ktoshima@users.noreply.github.com>
2024-01-11 16:14:53 +0900

    added options

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,27 +1,35 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './locales/en.json';
+import ja from './locales/ja.json';
 
 i18n
 	.use(initReactI18next)
 	.use(LanguageDetector)
 	.init({
-		fallbackLng: 'en', // set English default language
+		// set English default language
+		fallbackLng: 'en',
 		returnEmptyString: false,
 		resources: {
 			en: {
-				translation: require('./en.json')
+				translation: en
 			},
 			ja: {
-				translation: require('./ja.json')
+				translation: ja
 			}
 		},
+		// not needed for react as it escapes by default
 		interpolation: {
 			escapeValue: false
 		},
 		react: {
 			transKeepBasicHtmlNodesFor: ['br', 'strong', 'i', 'span']
-		}
+		},
+		// detect language from browser's language
+		detection: {
+			order: ['navigator'],
+		},
 	});
 
 export default i18n;
